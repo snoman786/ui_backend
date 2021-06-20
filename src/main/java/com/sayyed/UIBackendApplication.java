950f440d8f784dbbf3f596840918ee85d1a1713b
snoman786 <syedmohd.noman@gmail.com>
2021-06-20 17:42:01 +0530

    Initial API

--- src/main/java/com/sayyed/UIBackendApplication.java
+++ src/main/java/com/sayyed/UIBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class UIBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(UIBackendApplication.class, args);
 	}
 
 }
